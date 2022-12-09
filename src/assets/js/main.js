3fdcb2059458419196260593f942a9abeb07be42
Ali Babaee <alisource79@gmail.com>
2022-12-09 22:28:37 +0330

    12-9-2022

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,4 +6,26 @@ $(document).ready(()=>{
       $('.navbar-div').removeClass('sticky')
     }
   });
+  $('.owl-carousel').owlCarousel({
+    rtl: true,
+    loop: true,
+    responsiveClass: true,
+    nav: false,
+    dots: false,
+    autoWidth: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 8,
+      }
+    }
+  })
 })
